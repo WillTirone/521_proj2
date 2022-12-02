a11757d0d266b89e47d0e4bad365a14b93f10e67
Will Tirone <will.tirone1@gmail.com>
2022-12-02 17:39:43 -0500

    moving CV master to diff file, updating readme, updating project

--- CVmaster.R
+++ CVmaster.R
@@ -1,26 +1,22 @@
 
-
-#' Title
-#'
-#' @param classifier some text about classifier
-#' @param train_feat 
-#' @param train_labels 
-#' @param folds 
-#' @param loss 
-#'
-#' @return
-#' @export
-#'
-#' @examples
-CVmaster = function(classifier, train_feat, train_labels, folds=5, loss) {
+CVmaster = function(classifier, train_feat, train_labels, folds=5, loss,
+                    fit_type="response") {
   
+  # input: 
+    # classifier : generic fit object 
+    # train_feat : data frame object 
+    # train : data frame object with y labels, has column named "label"
+    # folds : number of k folds for cross validation 
+    # loss : generic loss function: 
+    # currently supports classification loss 
+    # fit_type : type of fit, valid values : {"response","class"}
   
-  # setting fit type for use in predictions later
-  if (fit$family$family == "binomial"){
-    fit_type = 'response'
-  } else {
-    fit_type = ''
-  }
+  # returns: 
+    # output : data frame with columns c("k","loss"), i.e. loss at each 
+    # fold k and not averaged over the k folds. To complete k-fold 
+    # cross validation, take mean of output$loss 
+  
+  train_labels = train_labels$label
   
   # data frame to output 
   output = data.frame(k = NA, loss = NA)
@@ -32,15 +28,28 @@ CVmaster = function(classifier, train_feat, train_labels, folds=5, loss) {
     
     # predict on validation set 
     predictions = predict(classifier,
-                          data.frame(train_feat[val[[i]],]),
+                          train_feat[val[[i]],],
                           type=fit_type)
     
+    # from probability calc label 
+    # need pred_labels to be -1 or 1 
+    if (fit_type == 'response'){
+      pred_labels = rep(-1, length(predictions))
+      pred_labels[predictions > 0.5] = 1
+    } else if (fit_type == 'class') {
+      pred_labels = predictions$class
+    } else {
+      print("fit not supported")
+    }
+    
     # calculate an arbitrary loss function 
-    L = loss(train_labels[val[[i]]], predictions)
+    L = loss(train_labels[val[[i]]], pred_labels)
     
     # then rbind 
     output = rbind(output, c(i,L))
+    
   }
   
-  return(output)
+  # remove null row 
+  return(output[-1,])
 }
--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# 521_proj2
+# STA 521: Project 2 Cloud Data
 Project 2 for STA 521, Statistical Learning
+
+Authors: Natalie Smith and Will Tirone 
+
+# Replication: 
+
+To clone the repository: 
+
+`git clone https://github.com/WillTirone/521_proj2.git`
+
+Additionally, the image files are large and cannot be uploaded to GitHub. Create 
+a folder in your working director after clone this repository titled `image_data`
+and add the images from the paper there.
+
+To run the main project file, `proj2.qmd`, simply open the file in your R 
+editor of choice and run all the cells. The project was completed with R version
+`4.1.3`. The libraries required are in the first cell of `proj2.qmd`, and are as
+follows if you don't have them installed: 
+
+library(factoextra)
+library(tidyverse)
+library(GGally)
+library(caret)
+library(lctools)
+library(sqldf)
+library(latex2exp)
+library(tidymodels)
+library(patchwork)
+library(tree)
+library(glmnet)
+library(MASS)
+library(xgboost)
+library(pROC)
+
